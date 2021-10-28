ee60ed755313efe58ba88e58bae013a760447dae
novb1492 <novb1492@naver.com>
2021-10-28 11:39:27 +0900

    상품정보/쿠폰/코드/포인트정보 가져오는 인터페이스

--- src/main/java/com/example/demo/product/model/getEventsAndProducts.java
+++ src/main/java/com/example/demo/product/model/getEventsAndProducts.java
@@ -1,5 +1,27 @@
 package com.example.demo.product.model;
 
+import java.sql.Timestamp;
+
 public interface getEventsAndProducts {
     
+    //상품정보
+    int getPrice();
+    String getProduct_name();
+    String getBig_kind();
+    //쿠폰정보
+    int getUsed_flag();
+    Timestamp getCo_expired();
+    String getCo_kind();
+    int getCo_num();
+    String getCoupon_name();
+    //코드정보
+    String getCode_name();
+    String getCd_kind();
+    int getCd_num();
+    int getCd_using();
+    Timestamp getCd_expired();
+    //포인트정보
+    int getPo_having();
+    Timestamp getPo_expired();
+
 }
